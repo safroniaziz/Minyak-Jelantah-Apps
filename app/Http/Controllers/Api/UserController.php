c4063e0bdfafb2b73db253c593a9b573baccb911
Jurusan Koding <jurusankoding@192.168.0.106>
2024-05-12 23:02:25 +0700

    new commit

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -61,7 +61,7 @@ class UserController extends Controller
             $request->validate([
                 'name' => ['required', 'string', 'max:255'],
                 'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-                'password' => $this->passwordRules()
+                'password' => 'required', 'string'
             ]);
 
             User::create([
